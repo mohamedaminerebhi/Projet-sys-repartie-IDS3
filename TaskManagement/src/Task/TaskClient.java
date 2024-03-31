f835c8e28911939014b0e6d2029c4b14d92f1d31
mohamedaminerebhi <159961750+mohamedaminerebhi@users.noreply.github.com>
2024-03-31 15:45:17 +0100

    Update TaskClient.java

--- TaskManagement/src/Task/TaskClient.java
+++ TaskManagement/src/Task/TaskClient.java
@@ -8,23 +8,43 @@ public class TaskClient {
 
   public static void main(String[] args) {
     try {
-      String url = "rmi://localhost:1000/TaskService";
+      String url = "rmi://localhost:195/TaskService";
       TaskService taskService = (TaskService) Naming.lookup(url);
 Scanner scanner =new Scanner(System.in);
-  while(true) {
-    String Task = scanner.nextLine();
-    if (Task.equals("no more Task"))
-      break;
-    taskService.addTask(Task);
-  }
+while (true) {
+    System.out.println("Que voulez-vous faire ? (1.ajouter/2.supprimer/3.afficher/4.no more Task)");
+    int action = scanner.nextInt();
+    scanner.nextLine();    
+    switch (action) {
+        case 1:
+            System.out.println("Entrez la tâche à ajouter :");
+            String newTask = scanner.nextLine();
+            taskService.addTask(newTask);
+            break;
+        case 2:
+            System.out.println("Entrez la tâche à supprimer :");
+            String taskToRemove = scanner.nextLine();
+            taskService.removeTask(taskToRemove);
+             break;
+        case 3:
+            List<String> tasks = taskService.getTasks();
+            System.out.println("Liste des tâches :");
+            for (String task : tasks) {
+                System.out.println("- " + task);
+            }
+            break;
+           
+        case 4:
+            System.out.println("Fin du programme.");
+            return;
+        default:
+            System.out.println("Commande non reconnue. Veuillez réessayer.");
+    }
+}
 
-      List<String> tasks = taskService.getTasks();
-      System.out.println("Liste des tâches :");
-      for (String task : tasks) {
-        System.out.println("- " + task);
-      }
+
+
     } catch (Exception e) {
       System.err.println("Erreur lors de l'accès au service RMI: " + e.getMessage());
     }
-  }
-}
+  }}
